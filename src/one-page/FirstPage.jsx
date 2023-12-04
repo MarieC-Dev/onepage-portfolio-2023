a95adaa65ecbefebea4a0f20e0d854fcd74aeefc
MarieC-Dev <marie.costa13@gmail.com>
2023-12-04 16:11:58 +0100

    resolve width to responsive

--- src/one-page/FirstPage.jsx
+++ src/one-page/FirstPage.jsx
@@ -7,18 +7,13 @@ import experiencesData from "./data/experiencesData";
 const FirstPage = () => {
   return (
     <div className="firstPage">
-      <p className="message">
-        Bonjour à tous ! Actuellement en train de développer mon site version
-        2023, je vous présente un premier aperçu sur une page. Mon portfolio
-        multi-page sera disponible prochainement sur ce même lien. Bonne visite
-        ! :)
-      </p>
-
       <header>
-        <h1>Marie Costa</h1>
-        <h2>Développeuse front-end</h2>
+        <div className="headerContent">
+          <h1>Marie Costa</h1>
+          <h2>Développeuse front-end</h2>
 
-        <p>Objectif : Développeuse full-stack</p>
+          <p>Objectif : Développeuse full-stack</p>
+        </div>
       </header>
 
       <main>
